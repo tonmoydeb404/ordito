0ffa2b47913ed1e7b345c7e02cf7a0a8f12057c3
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-11-07 01:15:18 +0600

    feat: removed unused files

--- src-tauri/src/state.rs
+++
@@ -1,6 +0,0 @@
-use crate::models::{CommandGroup, Schedule};
-use std::collections::HashMap;
-use std::sync::Mutex;
-
-pub type AppState = Mutex<HashMap<String, CommandGroup>>;
-pub type ScheduleState = Mutex<HashMap<String, Schedule>>;
